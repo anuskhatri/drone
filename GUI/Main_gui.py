67d07b31d0a49604eec4505054b50071305a3ac1
Adiittya <adityahakani29@gmail.com>
2023-12-20 20:32:21 +0530

    GUI

--- GUI/Main_gui.py
+++ GUI/Main_gui.py
@@ -15,6 +15,10 @@ import serial.tools.list_ports
 from geopy.geocoders import Nominatim
 from flight_operation import DroneController
 from CTkMessagebox import CTkMessagebox  
+import requests
+from gtts import gTTS
+import pygame
+import pyttsx3
 
 class Pop_up:
     def error(self,name, latitude  ,longitude):
@@ -65,7 +69,7 @@ class main_gui :
         connect_original_image = Image.open("C:\\Users\\Mr.Hetal Hakani\\Desktop\\SIH project\\drone\\GUI\\images\\connect_button.png")
         connect_resized_image = connect_original_image.resize((40, 40), Image.ANTIALIAS)
         self.connect_button_img = ImageTk.PhotoImage(connect_resized_image)
-
+        self.mail_status= True
         dis_original_image = Image.open("images\\disconnect_button.png")
         dis_resized_image = dis_original_image.resize((40, 40), Image.ANTIALIAS)
         self.dis_button_img = ImageTk.PhotoImage(dis_resized_image)
@@ -77,9 +81,6 @@ class main_gui :
         conn_cam_image = Image.open("images\\connected_cam.png")
         dis_resized_image = conn_cam_image.resize((40, 40), Image.ANTIALIAS)
         self.conn_cam_img = ImageTk.PhotoImage(dis_resized_image)
-
-
-
 
         self.screen_height=self.root.winfo_screenheight()
         self.screen_width=self.root.winfo_screenwidth()
@@ -114,10 +115,10 @@ class main_gui :
         self.com_port.place(x=1250,y=10)
 
         self.arm_button = ct.CTkButton(master = self.left_frame, text="Arm",command=lambda:threading.Thread(target= self.pixshawk_arm()))  # Connect/disconnect button 
-        self.arm_button.place(x=20 , y=550)
+        self.arm_button.place(x=20 , y=450)
 
         self.auto_land_button = ct.CTkButton(master = self.left_frame, text="Auto land")  # Connect/disconnect button 
-        self.auto_land_button.place(x=280 , y=550)
+        self.auto_land_button.place(x=280 , y=450)
 
  # ----------------------------------------------Dashboard label--------------------------------------------------------------- #
 
@@ -186,21 +187,13 @@ class main_gui :
         
         self.progressbar.set(0.5)
 
-# ----------------------------------------------Received Signal Strength Indication (RSSI)--------------------------------------------------------------- #
-
-        self.signal_strength_label = ct.CTkLabel(master=self.left_frame , text= "Received Signal Strength Indication (RSSI)", font=("", 20,) ) # Altitude label 
-        self.signal_strength_label.place(x= 20, y=450)
-
-        self.signal_strength= ct.CTkLabel(master=self.left_frame , text= "---", font=("", 25,)) # Altitude values 
-        self.signal_strength.place(x= 20, y=490)
-
 # ----------------------------------------------Online status--------------------------------------------------------------- #
         self.net_status = ct.CTkLabel(master= self.top_frame ,text="Online", text_color="#0FFF50",font=("", 20))
         self.net_status.place(x= 20, y=12)
 
         try:
             from socketTest import MySocketIOClient
-            self.server_url = 'https://2346-49-32-130-243.ngrok-free.app'
+            self.server_url = 'https://d66f-2409-40f0-1028-fc4b-b97b-ecb6-a4c8-96c5.ngrok-free.app'
             self.my_client = MySocketIOClient(self.server_url)
             self.my_client.start()
     
@@ -236,6 +229,8 @@ class main_gui :
         self.cam_connect_button = ct.CTkButton(master = self.right_frame, image= self.dis_cam_img, text="", fg_color="red",hover_color="green", corner_radius=500 ,width=10 ,height=10, command=self.call_threaded_cam)  # Connect/disconnect button 
         self.cam_connect_button.place(x=50, y=20) #camera button 
 
+        # self.cam_disconnect_button = ct.CTkButton(master = self.right_frame, image= self.dis_cam_img, text="", fg_color="green",hover_color="red", corner_radius=500 ,width=10 ,height=10, command=self.call_threaded_cam)  # Connect/disconnect button 
+        # self.cam_disconnect_button.place(x=50, y=70) #camera button 
 
         self.empty_cam_frame = ct.CTkFrame(master = self.right_frame , fg_color="black", width= 800 ,height =450)
         self.empty_cam_frame.place(x=125 ,y=70)
@@ -264,11 +259,29 @@ class main_gui :
         self.update_time_thread = threading.Thread(target=self.update_time)
         self.update_time_thread.start()
 
+        self.detected_object= []
+
         self.root.mainloop()
 
         
-    def stop_camera (self):
-        self.t1 = None
+    def stop_camera(self):
+    # You need to set a flag to stop the camera loop
+        # # self.is_detection_running = False
+
+        # # Release the camera
+        pass
+        # # self.cap.release()
+
+        # # Destroy the video view widget or update it as needed
+        # self.video_view.configure(image=None)
+
+        # # You may need to recreate or reinitialize other UI elements
+
+        # # Update the UI to show that the camera is disconnected
+        # # For example, recreate the connect button
+        # self.cam_disconnect_button = ct.CTkButton(master=self.right_frame, image=self.cam_disconnect_button, text="", fg_color="green", hover_color="red", corner_radius=500, width=10, height=10, command=self.call_threaded_cam)
+        # self.cam_disconnect_button.place(x=150, y=20)   
+
 
     def start_detection(self):
         self.is_detection_running = True
@@ -283,10 +296,10 @@ class main_gui :
         
         self.c1= threading.Thread(target=self.camera_connect())
         self.c1.start()
-        self.c1.daemon =True
+        # self.c1.daemon =True
         self.c1.join()
     def camera_connect(self):
-
+            self.cam_label.destroy()
             
             self.success, self.img = self.cap.read()
             result = self.model(self.img, stream=True)
@@ -295,42 +308,50 @@ class main_gui :
 
             if self.is_detection_running:
                 for r in result:
-                    boxes = r.boxes
-                    for box in boxes:                                                           
-                        x1, y1, x2, y2 = box.xyxy[0]
-                        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                        boxes = r.boxes
+                        for box in boxes:                                                           
+                            x1, y1, x2, y2 = box.xyxy[0]
+                            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
-                        w, h = x2 - x1, y2 - y1
-                        cvzone.cornerRect(self.img, (x1, y1, w, h))
+                            w, h = x2 - x1, y2 - y1
 
-                        conf = math.ceil((box.conf[0] * 100)) / 100
-                        cls = int(box.cls[0])
-                        self.label = f'{conf} {self.classes[cls]}'
-                        # print(label)
-                        if self.yolo_detect_object == "All":
-                                self.label = f'{conf} {self.classes[cls]}'
-                        elif(self.classes[cls] == self.yolo_detect_object.get()):
-                            if self.classes[cls] == self.yolo_detect_object.get():
-                                self.label = f'{conf} {self.classes[cls]}'
-                        cvzone.putTextRect(self.img, self.label, (max(0, x1), max(0, y1)))
 
+                            conf = math.ceil((box.conf[0] * 100)) / 100
+                            cls = int(box.cls[0])
+                            self.label = f'{conf} {self.classes[cls]}'
+                            # print(label)
+                            if self.yolo_detect_object.get() == "All":
+                                    self.label = f'{conf} {self.classes[cls]}'
+                                    cvzone.cornerRect(self.img, (x1, y1, w, h))
+                                    cvzone.putTextRect(self.img, self.label, (max(0, x1), max(0, y1)))
+
+                            elif(self.classes[cls] == self.yolo_detect_object.get()):
+                                if self.classes[cls] == self.yolo_detect_object.get():
+                                    self.label = f'{conf} {self.classes[cls]}'
+                                    cvzone.putTextRect(self.img, self.label, (max(0, x1), max(0, y1)))
             
-            new_width = 900  # Adjust the width as needed
-            aspect_ratio = self.img.shape[1] / self.img.shape[0]
-            new_height = int(new_width / aspect_ratio)
-    
 
-            self.img = cv2.resize(self.img, (new_width, new_height))
-            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
-            self.img = Image.fromarray(self.img)
-            self.img = ImageTk.PhotoImage(self.img)
+                    
 
-
-            self.video_view.configure(image=self.img)
+                new_width = 900  # Adjust the width as needed
+                aspect_ratio = self.img.shape[1] / self.img.shape[0]
+                new_height = int(new_width / aspect_ratio)
+                self.img = cv2.resize(self.img, (new_width, new_height))
+                self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+                self.img = Image.fromarray(self.img)
+                self.img = ImageTk.PhotoImage(self.img)
+                self.video_view.configure(image=self.img)
+                self.video_view.image = self.img
             
-            self.video_view.image = self.img
+                # print(class_counts)
+                # if self.mail_status:
+                #     self.call_mail()
+                self.root.after(10, self.camera_connect)
+              # Print the counts of each class
 
-            self.root.after(10, self.camera_connect)
+
+        
+
     
 #     def net(self):
 #         if netcheck.internet():
@@ -420,14 +441,34 @@ class main_gui :
                     geolocator = Nominatim(user_agent="location_extractor")
                     self.location = geolocator.reverse((self.latitude, self.longitude))
                     print(self.location)
+                    self.play_alert()
                     self.p1.error(self.name , self.latitude, self.longitude)
+
                     self.my_client.reset_alert_data()
             self.altitude.after(1000, self.alert_status)
             
+    def call_mail (self):
+
+
+        self.mail_status= False
+        self.send_emergency_mail()
+
+
+    def send_emergency_mail(self):
+        x = requests.get('http://10.12.4.128:4000/api/disaster/emergency_mail/5')    
 
         
 
-# mt =main_gui()
+    def play_alert(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load('call-to-attention-123107.mp3')
+        pygame.mixer.music.play()
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 100)  
+        engine.setProperty('volume',1.0) 
+        engine.say(f"Emergency alert has been recevied by {self.name} at location {self.location} " )
+        engine.runAndWait()
+        # mt =main_gui()
 m1= main_gui()
 
 
